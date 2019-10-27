738d98b86102f19ff0d172054e0597b355e3963a
Daniel Correa <danielfernandocorrea1@gmail.com>
2019-10-26 23:57:21 -0500

    i finish the problem with the verify the back account

---
+++ index.js
@@ -0,0 +1,40 @@
+var accounts_numbers_verify = 
+[
+    900800501,
+    632513563,
+    987632125,
+    412898914,
+    418241241,
+    769239537,
+    142984127,
+    142084120,
+    520937851,
+    199014124,
+    936840952,
+    529808712
+]
+
+var x = number();
+var y;
+
+
+function number(name) {
+    
+
+    var xaccount = document.getElementById("noaccount").value;
+    console.log(xaccount);
+
+    x = xaccount;
+    y = parseFloat(x);
+    
+    if(accounts_numbers_verify.includes(y)) {
+        alert("fkagoae");
+        console.log("eureka");
+        
+    }
+    else if (accounts_numbers_verify.includes(y) == false) {
+        alert ("ay cabron");
+        
+    }
+}
+
