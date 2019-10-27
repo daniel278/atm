33e8f380059dbf492e7a29d01eb0b6d206a7ba80
Daniel Correa <danielfernandocorrea1@gmail.com>
2019-10-27 17:28:55 -0500

    end the password code

--- index.js
+++ index.js
@@ -11,10 +11,31 @@ var accounts_numbers_verify =
     520937851,
     199014124,
     936840952,
-    529808712
+]
+
+var passwords = 
+[
+    213,
+    214,
+    634,
+    737,
+    373,
+    234,
+    474,
+    846,
+    242,
+    467,
+    145
 ]
 
 document.getElementById("send_accountnumber").addEventListener("click", number);
+document.getElementById("noaccount").addEventListener('keypress', function (e) {
+    var key = e.which || e.keyCode;
+    if (key === 13) { // 13 is enter
+      // code for enter
+      number();
+    }
+});
 
 function number() 
 {    
@@ -23,13 +44,18 @@ function number()
  
       x = xaccount;
       y = parseFloat(x);
+      v = accounts_numbers_verify.indexOf(y);
         
      if(accounts_numbers_verify.includes(y)) 
      {
-         document.getElementById("text").innerHTML = "Password";
-         document.getElementById("noaccount").value = "";
-         document.getElementById("atm").innerHTML = "<img src='../img/teclado.jpg'<div> <p>Pasword</p> <input id='number2' type='number'> <input id='send2' type='button' value='Send'></div>";
-        console.log("eureka");
+         document.getElementById("atm").innerHTML = "<img src='../img/teclado.jpg'<div> <p>Pasword</p> <input id='number2' type='number'> <input id='send2' onclick='password();' type='button' value='Send'></div>";
+         console.log("eureka");
+
+         if (accounts_numbers_verify[0] == y) 
+         {
+             z = passwords[0];
+             console.log(z);
+         }
             
     }
         else if (accounts_numbers_verify.includes(y) == false) {
@@ -37,4 +63,26 @@ function number()
     }
 }
 
+document.getElementById("number2").addEventListener('keypress', function (e) {
+    var key = e.which || e.keyCode;
+    if (key === 13) { // 13 is enter
+      // code for enter
+      password();
+    }
+});
+
+function password(params) 
+{
+    pass = document.getElementById("number2").value;
+    n = parseFloat(pass);
+    b = passwords.indexOf(n);
+
+    if (v == b) 
+    {
+        console.log("la verga");
+        
+    } else {
+        alert("INCORRECT PASSWORD");
+    };
+}
 
