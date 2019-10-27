0991c3347bf2807861e3db068d50849738854706
Daniel Correa <danielfernandocorrea1@gmail.com>
2019-10-27 00:11:37 -0500

    debug code

--- index.js
+++ index.js
@@ -16,39 +16,25 @@ var accounts_numbers_verify =
 
 document.getElementById("send_accountnumber").addEventListener("click", number);
 
-
-var flag = false;
-
-function start() {
-    flag = true;
-}
-
-
-    function number(name) 
-    {
-
-        if (flag) {
-    
-        var x;
-        var y;
-
-        var xaccount = document.getElementById("noaccount").value;
-        console.log(xaccount);
-    
-        x = xaccount;
-        y = parseFloat(x);
+function number() 
+{    
+      var xaccount = document.getElementById("noaccount").value;
+      console.log(xaccount);
+ 
+      x = xaccount;
+      y = parseFloat(x);
         
-        if(accounts_numbers_verify.includes(y)) {
-            alert("fkagoae");
-            console.log("eureka");
+     if(accounts_numbers_verify.includes(y)) 
+     {
+        alert("fkagoae");
+        console.log("eureka");
             
-        }
-        else if (accounts_numbers_verify.includes(y) == false) {
-            alert ("ay cabron");
-            
-        }
     }
-    }  
+        else if (accounts_numbers_verify.includes(y) == false) {
+        alert ("there is no this account number in our data base");
+    }
+}
+    
 
 
 
