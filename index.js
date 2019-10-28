213cd33bb30e05253ff7d27948abe1c870b2815c
Daniel Correa <danielfernandocorrea1@gmail.com>
2019-10-27 23:40:30 -0500

    create bills

--- index.js
+++ index.js
@@ -28,6 +28,42 @@ var passwords =
     145
 ]
 
+function bill(amount, value) 
+{
+    this.billAmount = amount;
+    this.billValue = value;
+}
+
+//I want the atm has equity in value of each bill
+
+equity();
+
+function equity(params) 
+{
+    totalvaluebills = 5000;
+    vhundred = 100;
+    xhundred = totalvaluebills/vhundred; // 50
+
+    vfifty = 50;
+    xfifty = totalvaluebills/vfifty;
+
+    vten = 10;
+    xten = totalvaluebills/vten,
+
+    vfive = 5;
+    xfive = totalvaluebills/vfive;
+
+    vone = 1;
+    xone = totalvaluebills/vone;
+
+}
+
+var onedollar = new bill(xone, vone);
+var fivedollar = new bill(xfive, vfive);
+var tendollar = new bill (xten, vten);
+var fiftydollar = new bill (xfifty, vfifty);
+var hundreddollar = new bill (xhundred, vhundred);
+
 document.getElementById("send_accountnumber").addEventListener("click", number);
 document.getElementById("noaccount").addEventListener('keypress',
 function (e) 
@@ -53,7 +89,8 @@ function number()
         //the code is for create a new text input and button, because they will do diferrents functions,
         // than the frist inpust. <p> have onclick, because, I find this way to js detect this <p>, 
         // before this, add.eventListener don't work  
-        document.getElementById("atm").innerHTML = "<img src='../img/teclado.jpg'<div> <p>Pasword</p> <input id='number2' onclick='problem();' type='number'> <input id='send2' onclick='password();' type='button' value='Send'></div>";
+        document.getElementById("image").src = "../img/teclado.jpg";
+        document.getElementById("atm").innerHTML = "<div id='key'> <p>Pasword</p> <input id='number2' onclick='problem();' type='number'> <input id='send2' onclick='password();' type='button' value='Send'> </div>";
         console.log("eureka");
 
         if (accounts_numbers_verify[0] == y) 
@@ -92,8 +129,7 @@ function password(params)
 
     if (v == b) 
     {
-        console.log("la verga");
-        
+        document.getElementById("atm").innerHTML = "<p>How many dollars you want?</p><input id='dollar' onclick='input3();' type='number'> <input id='give'type='button' onclick='give();' value='Give me'>";       
     } 
     
     else 
@@ -102,3 +138,32 @@ function password(params)
     };
 }
 
+function input3(params) 
+{
+//number2 is the id of the input put in innerHTML
+document.getElementById("dollar").addEventListener('keypress', function (e) 
+    {
+        var key = e.which || e.keyCode;
+        if (key === 13) 
+        { // 13 is enter
+        // code for enter
+        give();
+        }
+    });
+}
+
+function give(params) {
+    many = document.getElementById("dollar").value;
+
+    if (many%10 == 0) 
+    {
+        console.log("good");
+            
+    }
+    
+    else
+    {
+        alert("The ATM can't give you that amount of dollars, only multiples of ten");
+    }
+
+}
